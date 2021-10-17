49d2aa050da778be9d2d8230034446933cd7236a
johnfwebdev <john.f.webdev@gmail.com>
2021-10-17 01:50:37 -0500

    moved files

---
+++ cmd/createproducts.go
@@ -0,0 +1,13 @@
+package createprod
+
+import (
+	"fmt"
+)
+
+func createproduct() {
+	fmt.Println("Package: create_products")
+
+	endpoint := "https://api.bigcommerce.com"
+
+	fmt.Println(endpoint)
+}
